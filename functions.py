5ad1b87d02e2d7b5c8cd8686d7b29837bffbed79
Vicrobot <chaosplusorder@gmail.com>
2020-02-08 14:37:27 -0500

    Add expectimax and monte carlo versions

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
-import random
+from random import choice, random
 import numpy as np
 
-gen = lambda: random.choice([2]*9+[4]*1)
+gen = lambda: 2 if random() < .9 else 4
 scoregrid = np.asarray([     [4**15,4**14,4**13,4**12],
                              [4**8,4**9,4**10,4**11],
                              [4**7,4**6,4**5,4**4],
                              [4**0,4**1,4**2,4**3]])
+ind = np.arange(16).reshape(4,4)
+ind1 = np.arange(16)
 def left(l1):
     #assumption: l1 is 4 x 4 numpy matrix
     l = l1.copy()
@@ -28,6 +30,7 @@ def left(l1):
 #v[:,::-1]    #mirrored
 
 def c(l1, move):
+    #copyproof
     #assumption: l1 is 4 x 4 numpy matrix
     if move == 2: return left(l1)
     if move == 0: return left(l1[:,::-1].T).T[:,::-1] #anti, left, clock
@@ -36,38 +39,40 @@ def c(l1, move):
 
 
 def fillnums(l1, flat = 1):
+    #notcopyproof
     if 0 not in l1: return l1
     if flat != 1:
-        return fillnums(l1.flatten()).reshape(l1.shape)
-    l1[random.choice(np.arange(16)[l1==0])] = gen()
+        k = choice(ind[l1==0])
+        l1[k//4][k%4] = 2 if random() < .9 else 4
+    else:l1[choice(ind1[l1==0])] = 2 if random() < .9 else 4
     return l1
 
 def isvalid(l1):
+    #copyproof
     #assume l1 is 4 x 4
     if 0 in l1: return True
-    l = []
     for move in range(4):
         b = (l1 == c(l1,move)).all()
-        if not b: return True
-        l.append(b)
-    if all(l): return False
+        if not b:return True
+    return False
 
 def getChildren(data,playerT):
+    #copyproof
     if playerT:
         l = []
         for move in range(4):
-            #if (c(data.copy(), move) == data).all(): continue
-            l.append(c(data.copy(), move))
+            #if (c(data, move) == data).all(): continue
+            l.append(c(data, move))
     else:
         if 0 not in data: return [(data, 1)]
-        indices = (np.arange(16).reshape(4,4)[data == 0]).flatten()
+        indices = (ind[data == 0]).flatten()
         l = []
         for idx in indices:
             data1 = data.copy()
-            data1[idx//4][idx - 4*(idx//4)] = 2
+            p = idx - 4*(idx//4)
+            data1[idx//4][p] = 2
             l.append((data1,0.9))
-            data1 = data.copy()
-            data1[idx//4][idx - 4*(idx//4)] = 4
+            data1[idx//4][p] = 4
             l.append((data1,0.1))
     return l
 
@@ -113,18 +118,46 @@ def score_G(data):
 def next_play(l1, move):
     #assumption: l1 is 4 x 4 matrix
     first = c(l1, move)
+    if type(first)==type(None):return l1
     k = (first == l1).all()
     if 0 in first:
         if k: return l1
         else: return fillnums(first, flat = 0)
-    if not k: return (fillnums(first, flat = 0)) if valid else l1
-    else: return first if valid else l1
+    if not k: return (fillnums(first, flat = 0)) if isvalid(l1) else l1
+    else: return first if isvalid(l1) else l1
 
-def minimaxab(data, alpha, beta,depth, maximizing):
-    if depth <= 0 or not isvalid(data): return score_G(data.copy())
+def expectimax(data,depth, maximizing):
+    if depth <= 0 or not isvalid(data): return score_G(data)
     if maximizing:
         sc = float('-inf')
-        for child in getChildren(data.copy(), True): #will run 4 times at max
+        for child in getChildren(data, True): #will run 4 times at max
+            sc = max(sc, expectimax(child,depth-1,False))
+        return sc
+    if not maximizing: #chance node 
+        sc = 0
+        t = getChildren(data, False)
+        for child,w in t:
+            sc += w*expectimax(child,depth-1,True)
+        return sc/len(t)
+
+def monte_carlo(data,move,plays_c=10):
+    assert plays_c >0, 'Wrong value of plays_c'
+    score = 0
+    k = range(4)
+    for j in range(plays_c):
+        data1 = data.copy()
+        data1 = next_play(data1, move)
+        while isvalid(data1):
+            data1 = next_play(data1, choice(k))
+            score+= data1.max()
+    return score/plays_c
+
+def minimaxab(data, alpha, beta,depth, maximizing):
+    #funcs used: isvalid, score_G, getChildren
+    if depth <= 0 or not isvalid(data): return score_G(data)
+    if maximizing:
+        sc = float('-inf')
+        for child in getChildren(data, True): #will run 4 times at max
             sc = max(sc, minimaxab(child,alpha,beta, depth-1,False))
             if sc >= beta: return sc  #actually it is same as if alpha >= beta, 
                                         #maximizing player need not to care now #mamavsbhanja
@@ -132,11 +165,12 @@ def minimaxab(data, alpha, beta,depth, maximizing):
         return sc
     if not maximizing:
         sc = float('inf')
-        for child,w in getChildren(data.copy(), False):
+        for child,w in getChildren(data, False):
             sc = min(sc, minimaxab(child,alpha,beta, depth-1,True))
             if sc < alpha: return sc # minimizer need not to care
             beta = min(beta, sc)
         return sc
+
 
 def getMove(data, plays_c=4):
     sc, mv = float('-inf'), 5
@@ -155,6 +189,58 @@ def getMove(data, plays_c=4):
             mv = random.choice([mv, move])
     return mv
 
-    
+"""
+#Other versions of getMove, slight modifications:
 
+def getMove(data, plays_c=4):
+    #snake strategy with minimaxab
+    sc, mv = float('-inf'), 5
+    mx = data.max()
+    for move in [0,2,3]:#for move in range(4)  temp change
+        moved = c(data, move)
+        if mx == data[0][0] and moved.max() != moved[0][0]:continue #stuck the head; temp added
+        if ( moved == data).all(): continue
+        score = 0
+        score += minimaxab(moved, -np.inf, np.inf,plays_c, False)
+        #score += expectimax(moved, plays_c, False)     #if running expectimax; not much effect due to weak heuristics.
+        if move == 0 or move == 2: score += 10000 #motivation on up or left.
+        t_sc = score
+        if t_sc > sc:
+            sc= t_sc
+            mv = move
+        elif t_sc == sc:
+            if mv in [0,2]: continue
+            mv = choice([mv, move])
+    return mv
+
+def getMove(data, plays_c=4):              #monte carlo version
+    sc, mv = float('-inf'), 5
+    mx = data.max()
+    for move in [0,1,2,3]:
+        score = 0
+        #score += minimaxab(next_play(data.copy(), move),-np.inf, np.inf,plays_c, False)
+        score += monte_carlo(data.copy(),move,plays_c = plays_c)
+        t_sc = score
+        if t_sc > sc:
+            sc= t_sc
+            mv = move
+        elif t_sc == sc:
+            mv = choice([mv, move])
+    return mv
+
+Other modified heuristics:
+    div = 300
+    #a1 = -score/div if (data == 0).sum() < 3 else score/10 #free tiles
+    a1 = score/(div*(16-(data == 0).sum()))
+    a2 = score/div if data[0][0]>=data[1][0]>=data[2][0] else 0  #left vert.1 monotone
+    a3 = score/div if data[0][0]>=data[0][1]>=data[0][2]>=data[0][3] else -score/50 #top horz. monotone.
+    a4 = score/div if data[0][3] >= data[1][3] and data[1][3] >= data[1][2] else -score/30 #1to2 rightop corner monotone
+    a5 = 2*score/div if data.max() == data[0][0] else -3*score #max at top left.
+    a6 = score/div if data[0][1] >= data[1][1] >= data[2][1] else -score/3 #vert 2 mono
+    a7 = score/div if data[0][2] >= data[1][2] >= data[2][2] else -score/20  #vert 3 mono
+    a8 = score/div if data[2][2] <= data[1][2] else -score/20
+    
+"""
+
+    
 
