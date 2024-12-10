532dd9615efa09de0255ff3b828dd3dbb2c5e036
vicrobot <chaosplusorder@gmail.com>
2024-12-10 21:48:08 +0530

    Add config.py for configs for alternate development

---
+++ config.py
@@ -0,0 +1,14 @@
+grid_size = 4
+twos_chances = 0.9
+
+
+
+help_string = '"$python main.py <mode> <ai_level>": \nmode: 1 for manual play, mode: 0 for AI play.'
+default_ai_level_string = '"$python main.py <mode> <ai_level>" Use it to specify args.'
+wrong_mode_args_string = 'Wrong Arguments!\nMode must be 1 or 0. 1 for manual, 0 for AI play.'
+instructions_string = 'Press esc to exit anytime,\nUse up, down, left, right buttons to play'
+apocalypse_string = 'APOCALYPSE BEGINS'
+win_string = 'You Won !!!'
+initial_grid_string = 'f"    {row_vector[0]:>5}\t{row_vector[1]:>5}\t{row_vector[2]:>5}\t{row_vector[3]:>5}" f"\t{move_array[move]+ \' \'+str(count):<5}"'
+timestamp_string = 'f" T: {time.time()-self.time_start:>10.6}s"'
+post_initial_grid_string = 'f"    {row_vector[0]:>5}\t{row_vector[1]:>5}\t{row_vector[2]:>5}\t{row_vector[3]:>5}"'
